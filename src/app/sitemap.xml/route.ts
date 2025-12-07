b726d6e9c4d295adde862eee697b32b17bf7cfb0
Meike <meikehuettl@web.de>
2025-12-07 19:38:36 +0100

    Convert sitemap.xml to dynamic API route to fix Vercel caching issue

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,49 +1,49 @@
-<?xml version="1.0" encoding="UTF-8"?>
+// Dynamic sitemap.xml API route to avoid Vercel caching issues
+export async function GET() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <!-- Homepage -->
-  
   <url>
     <loc>https://imvestr.de/</loc>
     <lastmod>2025-12-07</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
-
-  <!-- Input Method -->
   <url>
     <loc>https://imvestr.de/input-method</loc>
     <lastmod>2025-12-07</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
-
-  <!-- Pricing -->
   <url>
     <loc>https://imvestr.de/pricing</loc>
     <lastmod>2025-12-07</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
-
-  <!-- Legal Pages -->
   <url>
     <loc>https://imvestr.de/impressum</loc>
     <lastmod>2025-12-07</lastmod>
     <changefreq>yearly</changefreq>
     <priority>0.3</priority>
   </url>
-
   <url>
     <loc>https://imvestr.de/datenschutz</loc>
     <lastmod>2025-12-07</lastmod>
     <changefreq>yearly</changefreq>
     <priority>0.3</priority>
   </url>
-
   <url>
     <loc>https://imvestr.de/agb</loc>
     <lastmod>2025-12-07</lastmod>
     <changefreq>yearly</changefreq>
     <priority>0.3</priority>
   </url>
-</urlset>
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=3600, s-maxage=3600',
+    },
+  });
+}
