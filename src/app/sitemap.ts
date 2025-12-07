eb108c3aaaa2b0f7da73a0860cadfdb96ecc9850
Meike <meikehuettl@web.de>
2025-12-07 19:50:02 +0100

    Fix domain in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://immovestr.de'
+  const baseUrl = 'https://imvestr.de'
   const currentDate = new Date()
 
   return [
